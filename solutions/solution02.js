558179fd3820610c25ace8f764e1afc4a2d91d90
Wender Z. Xavier <wender.zacariasxavier@gmail.com>
2020-06-06 20:23:05 -0300

    implement solution for problem 02

--- solutions/solution02.js
+++ solutions/solution02.js
@@ -1,5 +1,19 @@
 const solution02 = () => {
-    console.log("Solution 02");
-}
+    console.log("\nProblem 02:\nBy considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.");
+
+    const isEven = num => num % 2 == 0;
+
+    const fib = (previousValue = 1, previousPreviousValue = 2, currentSum = 2) => {
+        const currentValue = previousValue + previousPreviousValue;
+        if (currentSum < 4000000) {
+            if (isEven(currentValue)) {
+                currentSum += currentValue;
+            }
+            fib(previousPreviousValue, currentValue, currentSum);
+        } else console.log(currentSum);
+    }
+
+    fib();
+};
 
 exports.solution02 = solution02;
